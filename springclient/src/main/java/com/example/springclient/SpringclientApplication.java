0a3b4e430b73ac271c489f122ae0584d5143066e
tangliangdong <tld@betahouse.us>
2019-12-12 11:30:43 +0800

    first

---
+++ springclient/src/main/java/com/example/springclient/SpringclientApplication.java
@@ -0,0 +1,15 @@
+package com.example.springclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringclientApplication.class, args);
+    }
+
+}
